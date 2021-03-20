88f1b580f52d57d6b138d34b2d642398e09a3a2e
dpatelaqua <dipen@gringreen.com>
2021-03-20 14:10:13 -0500

    BI class

---
+++ Readme.cmd
@@ -0,0 +1,4 @@
+This is a Project Summary DashBoard using Power BI.
+https://app.powerbi.com/view?r=eyJrIjoiMDI0ZGUyOTMtNDQwOS00YjhjLWJmMTEtNjQwODNmOGY3M2MxIiwidCI6IjEwMWRhNTg3LTE4NDMtNGY1Mi04YjhhLTE3YjA2OWM2NmQzMyIsImMiOjJ9
+
+The data utilized is Data Analyst table
